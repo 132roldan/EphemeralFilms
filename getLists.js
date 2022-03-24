603f35250e611266ce31bf654c0cbbc65dc88709
Romualdo <132roldan@gmail.com>
2022-03-24 03:21:15 -0300

    I did kind of for but sitll problem with output

--- getLists.js
+++ getLists.js
@@ -7,24 +7,35 @@
 //     </section>
 //   </details>
 // </section>`
+function dbLenght(db){
+  let l = 0
+  firebase.firestore().collection(db).get().then((r)=>l = r.docs.length)
+  return l
+}
 
 const AllLists = document.getElementById('AllLists')
 let output = '';
 let secData_ = document.querySelector(`#${userx}`)
 async function test(){
  await firebase.firestore().collection('Lists').onSnapshot((qS)=>qS.forEach((doc)=>{
+   
   let user = (doc.data().user);
   let listname_ = (doc.data().listname);
-   firebase.firestore().collection(`${user}`)
+  console.log('usuario',user)
+  let l = dbLenght(user); 
+  let i = 0;
+   firebase.firestore().collection(user)
   .orderBy("timestamp").onSnapshot((qS)=>
     
-    qS.forEach((doc)=>{debugger
+    qS.forEach((doc)=>{
     
    output += `<li id='${doc.id}' >${doc.data().movie}</li>`;
   console.log("doc.id", doc.id)
-
-  }))
-  AllLists.innerHTML += `<section>
+  debugger
+      i ++
+      if(i > l){
+        debugger
+        AllLists.innerHTML += `<section>
   <details>
     <summary id="lista">${listname_}</summary>
     <section id='${userx}' class="filmesSelected">
@@ -35,6 +46,10 @@ async function test(){
 console.log('output: ',output)
   secData.innerHTML = output
 
+      }
+  }))
+  
+  
 }))
   
 }   test()
